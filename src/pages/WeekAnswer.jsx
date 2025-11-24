235715a3f28935f0d1d4a2cde973c0a0c3e9deab
Yeryeong Kang <dpfud8786@gmail.com>
2025-11-25 06:40:26 +0900

    docs: 기존 페이지 변경사항 삭제

--- src/pages/WeekAnswer.jsx
+++
@@ -1,9 +0,0 @@
-function WeekAnswer() {
-  return (
-    <div className="min-h-screen bg-bg-app flex items-center justify-center">
-      <p className="text-base text-text-main">WeekAnswer 메인 페이지 (임시)</p>
-    </div>
-  );
-}
-
-export default WeekAnswer;
