383e253120d97d72e5c73b62745121fe67cd9391
Yeryeong Kang <dpfud8786@gmail.com>
2025-12-04 19:16:59 +0900

    docs: 카카오 로그인 부분 배포 전 일단 주석 처리

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import "./index.css"; // ← 이 줄이 꼭 있어야 함!
 import { loadInitialTextSize } from "@/utils/textSize";
 
 // 🔹 카카오 SDK 초기화
-if (window.Kakao && !window.Kakao.isInitialized()) {
-  window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
-  // console.log("Kakao initialized", window.Kakao.isInitialized());
-}
+//if (window.Kakao && !window.Kakao.isInitialized()) {
+//  window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
+// console.log("Kakao initialized", window.Kakao.isInitialized());
+//}
 
 loadInitialTextSize(); // 저장된 글자 크기 설정을 html에 먼저 적용
 
